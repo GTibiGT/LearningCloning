30c5724684f23986769b95d24ffd6c890c394b64
Ibrahim Hussain <CompSciIbra@outlook.com>
2026-06-15 17:51:33 -0400

    adding question to 1D.cs

--- arrays/1D.cs
+++ arrays/1D.cs
@@ -101,3 +101,25 @@ public static int common(int[] arr) {
 		}
 		return 0;
 	}
+
+//Given a sorted array nums and a target, return the index of the target or -1 if it doesn't exist.
+//Input:  nums = [-1, 0, 3, 5, 9, 12], target = 9
+//Output: 4
+
+public static int findInt(int[] nums, int target) {
+		int high = nums.Length - 1;
+		int low = 0;
+		while (low <= high) {
+		    int mid = low + (high - low) / 2;
+			if (nums[mid] == target) {
+				return mid;
+			}
+			else if(nums[mid] < target) {
+				low = mid + 1;
+			}
+			else {
+				high = mid - 1;
+			}
+		}
+		return -1;
+}
